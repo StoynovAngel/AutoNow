71a4852fdad9cc90aba7c3f89541d362a9e350f1
Angel Stoynov <an.stoynov@gmail.com>
2025-11-29 19:24:53 +0200

    Initial commit

---
+++ backend/src/main/java/com/angel/autonow/AutoNowApplication.java
@@ -0,0 +1,13 @@
+package com.angel.autonow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoNowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoNowApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/angel/autonow/AutoNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angel.autonow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
