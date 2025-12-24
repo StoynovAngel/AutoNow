3357f931ea1120dba2f79e9f4f52b5bd669182a4
Stoynov <angel.stoynov@sap.com>
2025-12-24 12:13:08 +0200

    Reformat DTO

--- backend/src/main/java/com/angel/autonow/vehicle/VehicleDTO.java
+++ backend/src/main/java/com/angel/autonow/vehicle/VehicleDTO.java
@@ -1,11 +1,13 @@
 package com.angel.autonow.vehicle;
 
-public record VehicleDTO (
-	String brand,
-	String model,
-	String imageURL,
-	boolean airConditioning,
-	int numberOfSeats,
-	double trunkCapacity,
-	VehicleType vehicleType
-) { }
+public record VehicleDTO(
+		String brand,
+		String model,
+		String imageURL,
+		boolean airConditioning,
+		int numberOfSeats,
+		double trunkCapacity,
+		VehicleType vehicleType
+) {
+
+}
