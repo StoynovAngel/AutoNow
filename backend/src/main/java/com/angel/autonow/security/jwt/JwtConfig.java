5fcaa742af048ee660130041f1574ed34ab2b54a
Stoynov <angel.stoynov@sap.com>
2025-12-24 13:13:08 +0200

    Improve jwt config

--- backend/src/main/java/com/angel/autonow/security/jwt/JwtConfig.java
+++ backend/src/main/java/com/angel/autonow/security/jwt/JwtConfig.java
@@ -3,6 +3,7 @@ package com.angel.autonow.security.jwt;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
@@ -14,13 +15,16 @@ public class JwtConfig {
 	@Value("${jwt.secret}")
 	private String secret;
 
-	@Value("${jwt.algorithm}")
+	@Value("${jwt.algorithm:HmacSHA256}")
 	private String algorithm;
 
 	@Bean
 	public JwtDecoder jwtDecoder() {
 		byte[] bytes = secret.getBytes();
 		SecretKeySpec secretKeySpec = new SecretKeySpec(bytes, algorithm);
-		return NimbusJwtDecoder.withSecretKey(secretKeySpec).build();
+		return NimbusJwtDecoder
+				.withSecretKey(secretKeySpec)
+				.macAlgorithm(MacAlgorithm.HS256)
+				.build();
 	}
 }
