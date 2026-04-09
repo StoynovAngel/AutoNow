073b280c074c96f708ca2e6ac313f8f29591d4b8
Stoynov <angel.stoynov@sap.com>
2026-04-09 16:23:30 +0300

    Add rating service Add order repository as well. It needed, because both classes are closely related.

---
+++ backend/src/main/java/com/angel/autonow/rating/RatingService.java
@@ -0,0 +1,46 @@
+package com.angel.autonow.rating;
+
+import com.angel.autonow.order.OrderEntity;
+import com.angel.autonow.order.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class RatingService {
+
+	private final RatingRepository ratingRepository;
+	private final RatingMapper ratingMapper;
+	private final OrderRepository orderRepository;
+
+	public Optional<RatingResponseDTO> createRating(RatingRequestDTO request) {
+		Optional<OrderEntity> order = orderRepository.findById(request.orderId());
+
+		if (order.isEmpty()) {
+			return Optional.empty();
+		}
+
+		RatingEntity rating = ratingMapper.toEntity(request);
+		rating.setOrder(order.get());
+		RatingEntity saved = ratingRepository.save(rating);
+
+		return Optional.of(ratingMapper.toDTO(saved));
+	}
+
+	public Optional<RatingResponseDTO> getRatingById(Long id) {
+		return ratingRepository.findById(id).map(ratingMapper::toDTO);
+	}
+
+	public Optional<RatingResponseDTO> getRatingByOrderId(Long orderId) {
+		return ratingRepository.findByOrderId(orderId).map(ratingMapper::toDTO);
+	}
+
+	public List<RatingResponseDTO> getAllRatings() {
+		return ratingRepository.findAll().stream()
+				.map(ratingMapper::toDTO)
+				.toList();
+	}
+}
