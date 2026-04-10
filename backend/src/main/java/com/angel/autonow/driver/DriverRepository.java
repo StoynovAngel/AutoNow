52156a39e8e479a09d780bd9a66c4f631fff95fc
Stoynov <angel.stoynov@sap.com>
2026-04-10 18:35:03 +0300

    Add search by company id

--- backend/src/main/java/com/angel/autonow/driver/DriverRepository.java
+++ backend/src/main/java/com/angel/autonow/driver/DriverRepository.java
@@ -3,10 +3,13 @@ package com.angel.autonow.driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DriverRepository extends JpaRepository<DriverEntity, Long> {
 
 	Optional<DriverEntity> findByLicenseNumber(String licenseNumber);
+
+	List<DriverEntity> findByCompanyId(Long companyId);
 }
--- backend/src/main/java/com/angel/autonow/vehicle/VehicleRepository.java
+++ backend/src/main/java/com/angel/autonow/vehicle/VehicleRepository.java
@@ -3,7 +3,10 @@ package com.angel.autonow.vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VehicleRepository extends JpaRepository<VehicleEntity, Long> {
 
+	List<VehicleEntity> findByCompanyId(Long companyId);
 }
