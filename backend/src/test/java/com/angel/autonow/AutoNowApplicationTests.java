b74f3ca3ee71c4c32f872b08b239d94ce0f3bd4d
Stoynov <angel.stoynov@sap.com>
2026-04-14 21:26:19 +0300

    Add cors for the test application properties #2

--- backend/src/test/java/com/angel/autonow/AutoNowApplicationTests.java
+++ backend/src/test/java/com/angel/autonow/AutoNowApplicationTests.java
@@ -2,8 +2,12 @@ package com.angel.autonow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"cors.allowed.origins=http://localhost:8080,http://localhost:8081"
+})
 class AutoNowApplicationTests {
 
 	@Test
