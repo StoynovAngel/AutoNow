caafe9cb73b5d0e82d8451e83cb38b0a25bfce4c
Angel Stoynov <164257258+StoynovAngel@users.noreply.github.com>
2026-04-18 12:30:28 +0300

    Fix connectivity issue (#51)
    Co-authored-by: Stoynov <angel.stoynov@sap.com>

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -4,6 +4,10 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: config.name ?? "AutoNow",
   slug: config.slug ?? "autonow",
+  android: {
+    ...config.android,
+    usesCleartextTraffic: true,
+  },
   extra: {
     ...(process.env.API_URL ? { apiUrl: process.env.API_URL } : {}),
   },
