dc5df5bf369104e859c8a52cb3bd1da6a1de2d31
Stoynov <angel.stoynov@sap.com>
2026-04-30 12:49:30 +0300

    Apply everything to app.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import Navigation from './src/navigation/Navigation';
+import {I18nextProvider} from 'react-i18next';
+import i18n from "../frontend/src/config/i18n";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <I18nextProvider i18n={i18n}>
+                <Navigation/>
+            </I18nextProvider>
+        </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
