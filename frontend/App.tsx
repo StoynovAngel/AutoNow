eaee091e35ef3313481fb1376cd0c68c13e8b549
I765313 <angel.stoynov@sap.com>
2026-06-10 17:19:28 +0300

    Remove theme provider

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,7 +3,6 @@ import {NavigationContainer} from '@react-navigation/native';
 import Navigation from './src/navigation/Navigation';
 import {I18nextProvider} from 'react-i18next';
 import i18n from "../frontend/src/config/i18n";
-import {ThemeProvider} from './src/hooks/useTheme';
 import {AuthProvider} from './src/services/AuthContext';
 import {PaperProvider} from 'react-native-paper';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
@@ -14,15 +13,13 @@ export default function App() {
         <ErrorBoundary>
             <SafeAreaProvider>
                 <PaperProvider>
-                    <ThemeProvider>
-                        <AuthProvider>
-                            <NavigationContainer>
-                                <I18nextProvider i18n={i18n}>
-                                    <Navigation/>
-                                </I18nextProvider>
-                            </NavigationContainer>
-                        </AuthProvider>
-                    </ThemeProvider>
+                    <AuthProvider>
+                        <NavigationContainer>
+                            <I18nextProvider i18n={i18n}>
+                                <Navigation/>
+                            </I18nextProvider>
+                        </NavigationContainer>
+                    </AuthProvider>
                 </PaperProvider>
             </SafeAreaProvider>
         </ErrorBoundary>
